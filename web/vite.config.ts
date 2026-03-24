c32426f192236598f60b993085fe8189b7e96370
Seçkin Ozan Uyan <ozanuyan@gmail.com>
2026-03-24 17:06:12 +0300

    eh more ui shenanigans, just using one png is a pain...

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,9 +2,14 @@ import path from "path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({ targets: [{ src: 'src/assets/icons', dest: 'assets' }] })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
